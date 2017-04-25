0df3f141311633120d90e070c1416d73c8dcc4fb
Daniel Gerlach <wrack1981@gmx.de>
2017-04-25 18:29:58 +0200

    formatting

--- src/index.ts
+++ src/index.ts
@@ -14,31 +14,31 @@ server.on('listening', onListening);
 server.listen(port);
 
 function normalizePort(val: number | string): number | string | boolean {
-    let port: number = (typeof val === 'string') ? parseInt(val, 10) : val;
-    if (isNaN(port)) return val;
-    else if (port >= 0) return port;
-    else return false;
+  let port: number = (typeof val === 'string') ? parseInt(val, 10) : val;
+  if (isNaN(port)) return val;
+  else if (port >= 0) return port;
+  else return false;
 }
 
 function onError(error: NodeJS.ErrnoException): void {
-    if (error.syscall !== 'listen') throw error;
-    let bind = (typeof port === 'string') ? 'Pipe ' + port : 'Port ' + port;
-    switch (error.code) {
-        case 'EACCES':
-            console.error(`${bind} requires elevated priviliges`);
-            process.exit(1);
-            break;
-        case 'EADDRINUSE':
-            console.log(`${bind} is already in use`);
-            process.exit(1);
-            break;
-        default:
-            throw error;
-    }
+  if (error.syscall !== 'listen') throw error;
+  let bind = (typeof port === 'string') ? 'Pipe ' + port : 'Port ' + port;
+  switch (error.code) {
+  case 'EACCES':
+    console.error(`${bind} requires elevated priviliges`);
+    process.exit(1);
+    break;
+  case 'EADDRINUSE':
+    console.log(`${bind} is already in use`);
+    process.exit(1);
+    break;
+  default:
+    throw error;
+  }
 }
 
 function onListening(): void {
-    let addr = server.address();
-    let bind = (typeof addr === 'string') ? `pipe ${addr}` : `port ${addr.port}`;
-    debug(`Listening on ${bind}`);
+  let addr = server.address();
+  let bind = (typeof addr === 'string') ? `pipe ${addr}` : `port ${addr.port}`;
+  debug(`Listening on ${bind}`);
 }
